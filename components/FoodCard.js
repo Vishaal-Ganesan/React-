52f906fec935e5153a9ef98a8147cc4e26458b94
=Vishaal <=vishaal@clovation.io>
2023-12-05 23:32:51 +0530

    05night

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -1,68 +1,27 @@
 import { TextField,Icon, Card, CardContent, Typography, AppBar, Stack, createTheme } from '@mui/material'
 import React from 'react'
 import '../App.css'
-import data from './'
+import {restaurants} from './constants/data';
 
 const FoodCard = () => {
+  const RES_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
   return (
     <div className='food-cards'>
         <Stack direction='row' flexWrap='wrap'>
-          <Card sx={{minWidth:200,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:450}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-
-          {/* <Card sx={{minWidth:300,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:450}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{minWidth:300,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:450}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{minWidth:300,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:400}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{minWidth:300,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:400}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{minWidth:300,marginTop:'100px'}}>
-            <CardContent>
-              <img style={{width:400}} src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/uvey2fbfg7zwahbtxixs" alt="biriyni" />
-              <Typography>The Biryani Mall</Typography>
-              <Typography>⭐4.3 . 20mins</Typography>
-              <Typography>Biryani</Typography>
-              <Typography>Coimbatore</Typography>
-            </CardContent>
-          </Card>
-           */}
+          {restaurants.map((restaurant)=>(
+              <Card sx={{background:'grey',minWidth:200,marginTop:'100px',marginLeft:8.5}}>
+              <CardContent>
+                <img style={{width:'330px',height:'200px',objectFit:'cover',borderRadius:10}} src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurant?.info?.cloudinaryImageId}`} alt={restaurant?.info?.name} />
+                <Typography sx={{fontSize:20,fontWeight:'bold'}}>{restaurant?.info?.name}</Typography>
+                <Typography sx={{fontSize:20,fontWeight:'bold'}}><span style={{padding:2,borderRadius:'50%'}}>⭐</span>{restaurant?.info?.avgRating} • {restaurant?.info?.sla?.slaString}</Typography>
+                <Typography sx={{fontSize:20,fontWeight:'lighter'}}>{restaurant?.info?.cuisines?.map((cuisine,index)=>{
+                  return <span key={index}>{index===0 || index === restaurant?.info?.cuisines.length-1? '':','}{cuisine}</span>
+                })}</Typography>
+                <Typography sx={{fontSize:20,fontWeight:'lighter'}}>Coimbatore</Typography>
+              </CardContent>
+            </Card>  
+          ))}
           </Stack>
 
     </div>
